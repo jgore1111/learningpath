4e2b67a5228853061b04d8cea4701a4eba393269
Jo Gore <paratopia@protonmail.com>
2019-04-23 19:38:00 -0500

    Initial commit

---
+++ learning.js
@@ -0,0 +1,49 @@
+console.log("Hola Mundo");
+//Comentario de una línea
+/*
+Comentario
+multi
+linea
+*/
+
+//Tipos de datos
+/*
+string
+number
+booleano
+*/
+//String -> " " v ' '
+//Number -> 42, 33, 1e6, 1.5
+//Booleano -> true  false
+
+//Operadores:
+/*
+aritméticos
+lógicos
+igualdad
+*/
+//Aritméticos -> +, -, *, /, %
+//Lógicos ->
+/* 
+||: or
+&&: and
+!: not
+*/
+//Igualdad -> ==, ===
+/*
+== igualdad sin importar el tipo de dato
+sólo la representación
+==== igualdad de tipo de dato y de representación
+*/
+
+//Funciones:
+/*typeof: identifica qué tipo de dato es el argumento*/
+
+//Variables
+/* 
+-var(palabra reservada): var nombre =" contenido"
+                       : permite reasignación de valor
+-let: let nombre2 ="contenido"
+    :reasignación de valor
+-const: const nombre3 = 4
+      :no permite reasignación*/
---
+++ readme.md
@@ -0,0 +1,16 @@
+# LearningPath
+
+## Crear cuenta en GitHub
+
+- Nombrar repo
+
+- Crear repo con ssh
+
+- Creación de una llave (ohmyzsh: ssh keygen )
+
+- Obtener la llave (cat ./.ssh/id_rsa.pub )[Siempre ponder .pub para solo mostrar llave pública]
+
+- Copiar la llave a Git (en ícono, settings, ssh and gpg keys, new sshkey y copiar la llave)
+- Clonar un repositorio en el Term (copiar el URL en T con `git clone + url`)
+
+##Hacer pruebas en la T (`node`, escribir en la T )
